a584bcc9c1b12cb14a998b7875959ec6827b9dc9
Anand Sharma <anasharm@cisco.com>
2020-02-09 10:26:07 -0500

    Changing default memory values per VM

--- run-xenial.sh
+++ run-xenial.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 VM_NAME="${1:-xenial}"
-multipass launch -n ${VM_NAME} -c 2 -m 1G --cloud-init $HOME/workspace/localvm-scripts/xenial.yaml xenial
+multipass launch -n ${VM_NAME} -c 2 -m 2G --cloud-init $HOME/workspace/localvm-scripts/xenial.yaml xenial
